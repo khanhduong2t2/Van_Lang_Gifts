9c1e011b079ab9d304ac20c0970d055972cd4dda
khanhduong2t2 <khanhduong2t2@gmail.com>
2023-08-04 10:02:03 +0700

    [Duong] Update vercel 2

--- src/s3.js
+++ src/s3.js
@@ -2,10 +2,10 @@ require('dotenv').config();
 const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
 
-const bucketName = process.env.AWS_BUCKET_NAME;
-const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
-const secretAccessKey = process.env.AWS_SECRET_KEY;
+const bucketName = process.env.BUCKET_NAME_AWS;
+const region = process.env.BUCKET_REGION_AWS;
+const accessKeyId = process.env.ACCESS_KEY_AWS;
+const secretAccessKey = process.env.SECRET_KEY_AWS;
 
 const s3 = new S3({
     region,
